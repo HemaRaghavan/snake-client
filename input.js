c12b0fe951fbc8c17daf9b6574ba35031103db71
HemaRaghavan <pvhemaraghavan@gmail.com>
2020-09-24 01:44:15 +0000

    movements

--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+// Stores the active TCP connection object.
+let connection;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +13,14 @@ const setupInput = function() {
   stdin.on('data', handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === '\u0077') {
+      conn.write('Move: up');
+    } else if (key === '\u0061') {
+      conn.write('Move: left');
+    } else if (key === '\u0073') {
+      conn.write('Move: down');
+    } else if (key === '\u0064') {
+      conn.write('Move: right');
     }
   });
   return stdin;
